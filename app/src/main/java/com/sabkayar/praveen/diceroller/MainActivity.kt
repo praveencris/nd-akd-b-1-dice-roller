8573635eaa8603f131d8342e6a214e417aa9ba2c
Praveen <pkchmits@gmail.com>
2020-11-30 20:36:40 +0530

    Done with exercies 3

--- app/src/main/java/com/sabkayar/praveen/diceroller/MainActivity.kt
+++ app/src/main/java/com/sabkayar/praveen/diceroller/MainActivity.kt
@@ -2,19 +2,26 @@ package com.sabkayar.praveen.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton:Button = findViewById(R.id.roll_button)
-        rollButton.text="Let's Roll It"
+        val rollButton: Button = findViewById(R.id.roll_button)
 
-        rollButton.setOnClickListener(){
-            Toast.makeText(this,"Button Clicked!",Toast.LENGTH_SHORT).show()
+        rollButton.setOnClickListener() {
+            rollDice()
         }
 
+    }
+
+    private fun rollDice() {
+        val number: Int = Random.nextInt(6) + 1
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = number.toString()
     }
 }
