a84c0f3706d3c5ff82138577ab69662582594b6b
Praveen <pkchmits@gmail.com>
2020-11-30 20:50:30 +0530

    Exercise 4 done Use of when learnt use of Random variable setup ImageResource to ImageView

--- app/src/main/java/com/sabkayar/praveen/diceroller/MainActivity.kt
+++ app/src/main/java/com/sabkayar/praveen/diceroller/MainActivity.kt
@@ -2,8 +2,7 @@ package com.sabkayar.praveen.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.random.Random
 
@@ -21,7 +20,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val number: Int = Random.nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = number.toString()
+        val diceImageView: ImageView = findViewById(R.id.dice_Image)
+        val drawableResource = when (number) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImageView.setImageResource(drawableResource)
     }
 }
